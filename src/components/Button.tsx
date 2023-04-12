0ae8f6518c8b4571b8dadbface5187c264841f6c
Matej Tarca <matej.tarca@kiwi.com>
2023-04-12 21:06:36 +0200

    chore(storybook): configure storybook to work with tailwindcss

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./button.css";
 
 interface ButtonProps {
   /**
@@ -32,25 +31,15 @@ export const Button = ({
   size = "medium",
   backgroundColor,
   label,
-  ...props
+  onClick,
 }: ButtonProps) => {
-  const mode = primary
-    ? "storybook-button--primary"
-    : "storybook-button--secondary";
   return (
     <button
       type="button"
-      className={["storybook-button", `storybook-button--${size}`, mode].join(
-        " "
-      )}
-      {...props}
+      className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+      onClick={onClick}
     >
       {label}
-      <style jsx>{`
-        button {
-          background-color: ${backgroundColor};
-        }
-      `}</style>
     </button>
   );
 };
