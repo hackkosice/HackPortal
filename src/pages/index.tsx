2016565295bed7b11633390a61bde1c471208895
Matej Tarca <matej.tarca@kiwi.com>
2023-04-23 20:12:41 +0200

    feat(components): add icons package

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/Button";
+import { BoltIcon } from "@heroicons/react/24/solid";
 
 export default function Home() {
   return (
     <div>
       <h1 className="font-title">HomePage</h1>
       <Button label="OK" primary />
+      <BoltIcon className="h-6 w-6 text-blue-500" />
     </div>
   );
 }
