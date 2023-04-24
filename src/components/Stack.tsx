d4c88f43cf4595cbb75bd47c06678c609e798c31
Matej Tarca <matej.tarca@kiwi.com>
2023-04-24 22:08:41 +0200

    feat(storybook): add Stack component

---
+++ src/components/Stack.tsx
@@ -0,0 +1,43 @@
+import React, { PropsWithChildren, useMemo } from "react";
+import { Size } from "@/components/types";
+
+type Direction = "row" | "column";
+
+export type StackProps = PropsWithChildren<{
+  spacing?: Size;
+  direction?: Direction;
+}>;
+
+const getDirectionClasses = (direction?: Direction) => {
+  switch (direction) {
+    case "column":
+      return "flex-col";
+    default:
+      return "flex-row";
+  }
+};
+
+const getSpacingClasses = (spacing?: Size) => {
+  switch (spacing) {
+    case "small":
+      return "gap-2";
+    case "large":
+      return "gap-7";
+    default:
+      return "gap-4";
+  }
+};
+
+const STACK_BASE_CLASSES = "flex";
+
+export const Stack = ({ children, direction, spacing }: StackProps) => {
+  const computedClasses = useMemo(() => {
+    const directionClasses = getDirectionClasses(direction);
+    const spacingClasses = getSpacingClasses(spacing);
+
+    return [directionClasses, spacingClasses].join(" ");
+  }, [direction, spacing]);
+  return (
+    <div className={`${STACK_BASE_CLASSES} ${computedClasses}`}>{children}</div>
+  );
+};
---
+++ src/stories/Stack.stories.tsx
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Stack } from "../components/Stack";
+import { InputText } from "../components/InputText";
+import { Button } from "../components/Button";
+
+const meta: Meta<typeof Stack> = {
+  title: "Layout/Stack",
+  component: Stack,
+  tags: ["autodocs"],
+};
+
+export default meta;
+type Story = StoryObj<typeof Stack>;
+
+const StackChildren = [
+  // eslint-disable-next-line react/jsx-key
+  <InputText label="Username" />,
+  // eslint-disable-next-line react/jsx-key
+  <InputText label="Password" type="password" />,
+  // eslint-disable-next-line react/jsx-key
+  <Button label={"Log in"} />,
+];
+
+export const Default: Story = {
+  args: {
+    children: StackChildren,
+    spacing: "medium",
+    direction: "column",
+  },
+};
