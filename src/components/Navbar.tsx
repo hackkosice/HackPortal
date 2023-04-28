bbf7c3dae93721df92f239727f7a0ee3a59e4359
Matej Tarca <matej.tarca@kiwi.com>
2023-04-28 22:13:14 +0200

    feat(pages): add basic layout and LandingPage

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ import { useBreakpoints } from "../services/hooks/useBreakpoints";
 const Navbar = () => {
   const { isMedium } = useBreakpoints();
   return (
-    <nav className="flex flex-row bg-hkLightGray w-full m-0 py-3 px-3 border-b-2 border-hkGray content-center md:py-6 md:px-5">
+    <nav className="fixed flex flex-row bg-hkLightGray w-full m-0 py-3 px-3 border-b-2 border-hkGray content-center md:py-6 md:px-5">
       <Link href={"/"} className="flex content-center">
         <Image
           src="hk-logo.svg"
