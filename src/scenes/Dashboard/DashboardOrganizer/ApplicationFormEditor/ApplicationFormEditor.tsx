8218f08346cadbc788f309022f029ed7ff946ec3
Matej Tarca <matej.tarca@kiwi.com>
2023-05-01 13:33:51 +0200

    feat(form-editor): add steps edit and delete

--- src/scenes/Dashboard/DashboardOrganizer/ApplicationFormEditor/ApplicationFormEditor.tsx
+++ src/scenes/Dashboard/DashboardOrganizer/ApplicationFormEditor/ApplicationFormEditor.tsx
@@ -1,11 +1,25 @@
+import React, { useState } from "react";
 import { Card } from "@/components/Card";
 import { Heading } from "@/components/Heading";
 import { Text } from "@/components/Text";
 import { trpc } from "@/services/trpc";
 import { Button } from "@/components/Button";
 import Step from "./components/Step";
+import { Modal } from "@/components/Modal";
+import { InputText } from "@/components/InputText";
+import { Stack } from "@/components/Stack";
+import { useForm } from "react-hook-form";
+
+type TitleEditForm = {
+  title: string;
+};
 
 const ApplicationFormEditor = () => {
+  const [isEditTitleModalOpened, setIsEditTitleModalOpened] = useState(false);
+  const [editingStepId, setEditingStepId] = useState<number | null>(null);
+  const { register: registerTitleEdit, handleSubmit } =
+    useForm<TitleEditForm>();
+
   const { data, isLoading } = trpc.steps.useQuery();
   const utils = trpc.useContext();
   const { mutateAsync: newStep } = trpc.newStep.useMutation({
@@ -18,32 +32,73 @@ const ApplicationFormEditor = () => {
       utils.steps.invalidate();
     },
   });
+  const { mutateAsync: deleteStep } = trpc.deleteStep.useMutation({
+    onSuccess: () => {
+      utils.steps.invalidate();
+    },
+  });
 
   const createNewStep = () => {
     newStep();
   };
 
   const onStepEdit = async (stepId: number) => {
-    editStep({ id: stepId, title: "New title" });
+    setIsEditTitleModalOpened(true);
+    setEditingStepId(stepId);
+  };
+
+  const onEditTitleModalSave = async (data: TitleEditForm) => {
+    setIsEditTitleModalOpened(false);
+    editStep({ id: editingStepId as number, title: data.title });
+    setEditingStepId(null);
+  };
+
+  const onEditTitleModalClose = async () => {
+    setIsEditTitleModalOpened(false);
+    setEditingStepId(null);
+  };
+
+  const onStepDelete = async (stepId: number) => {
+    deleteStep({ id: stepId });
   };
 
   return (
-    <Card>
-      <Heading centered spaceAfter="medium">
-        Application Form Editor
-      </Heading>
-      {isLoading && <Text>Loading...</Text>}
-      {data?.data.map(({ title, id, stepNumber }) => (
-        <Step
-          key={id}
-          title={title}
-          stepNumber={stepNumber}
-          onEdit={() => onStepEdit(id)}
-        />
-      ))}
-      <br />
-      <Button label="Create new step" onClick={createNewStep} />
-    </Card>
+    <>
+      <Modal
+        isOpened={isEditTitleModalOpened}
+        closeOnOutsideClick={true}
+        onClose={onEditTitleModalClose}
+      >
+        <form onSubmit={handleSubmit(onEditTitleModalSave)}>
+          <Stack direction="column">
+            <InputText
+              label="New title"
+              register={registerTitleEdit}
+              name="title"
+              required
+            />
+            <Button type="submit" label={"Save"} />
+          </Stack>
+        </form>
+      </Modal>
+      <Card>
+        <Heading centered spaceAfter="medium">
+          Application Form Editor
+        </Heading>
+        {isLoading && <Text>Loading...</Text>}
+        {data?.data.map(({ title, id, stepNumber }) => (
+          <Step
+            key={id}
+            title={title}
+            stepNumber={stepNumber}
+            onEdit={() => onStepEdit(id)}
+            onDelete={() => onStepDelete(id)}
+          />
+        ))}
+        <br />
+        <Button label="Create new step" onClick={createNewStep} />
+      </Card>
+    </>
   );
 };
 
--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -4,6 +4,7 @@ import userInfo from "@/server/routers/userInfo";
 import steps from "@/server/routers/organizerDashboard/formEditor/steps";
 import newStep from "@/server/routers/organizerDashboard/formEditor/newStep";
 import editStep from "@/server/routers/organizerDashboard/formEditor/editStep";
+import deleteStep from "@/server/routers/organizerDashboard/formEditor/deleteStep";
 
 export const appRouter = router({
   signup,
@@ -11,6 +12,7 @@ export const appRouter = router({
   steps,
   newStep,
   editStep,
+  deleteStep,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
