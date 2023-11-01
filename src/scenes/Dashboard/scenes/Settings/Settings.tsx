fc90ffa98155fd93892d9c883c22dd2564eb0ddf
Matej Tarca <matej.tarca@kiwi.com>
2023-11-01 09:08:58 +0100

    feat(voting): add vote parameter creation

--- src/scenes/Dashboard/scenes/Settings/Settings.tsx
+++ src/scenes/Dashboard/scenes/Settings/Settings.tsx
@@ -1,15 +1,28 @@
 import React from "react";
-import { Card, CardContent } from "@/components/ui/card";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { Stack } from "@/components/ui/stack";
 
-const Settings = () => {
+type SettingsProps = {
+  hackathonId: number;
+};
+
+const Settings = ({ hackathonId }: SettingsProps) => {
   return (
     <Card>
+      <CardHeader></CardHeader>
       <CardContent>
-        <Button asChild>
-          <Link href="/option-lists">Manage option lists</Link>
-        </Button>
+        <Stack direction="column">
+          <Button asChild>
+            <Link href="/option-lists">Manage option lists</Link>
+          </Button>
+          <Button asChild>
+            <Link href={`/dashboard/${hackathonId}/settings/vote-parameters`}>
+              Manage vote parameters
+            </Link>
+          </Button>
+        </Stack>
       </CardContent>
     </Card>
   );
