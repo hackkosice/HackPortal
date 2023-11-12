d21f87bab5ac6d767e3c5ba0e2fa73b9d55f5c6b
Matej Tarca <matej.tarca@kiwi.com>
2023-11-12 18:22:26 +0100

    feat(db): add descriptions to step and formfield

---
+++ prisma/migrations/20231112172128_step_and_field_descriptions/migration.sql
@@ -0,0 +1,27 @@
+-- AlterTable
+ALTER TABLE "ApplicationFormStep" ADD COLUMN "description" TEXT;
+
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_FormField" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "position" INTEGER NOT NULL,
+    "label" TEXT NOT NULL,
+    "description" TEXT,
+    "name" TEXT NOT NULL,
+    "required" BOOLEAN NOT NULL,
+    "shownInList" BOOLEAN NOT NULL DEFAULT false,
+    "stepId" INTEGER NOT NULL,
+    "typeId" INTEGER NOT NULL,
+    "optionListId" INTEGER,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "FormField_stepId_fkey" FOREIGN KEY ("stepId") REFERENCES "ApplicationFormStep" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "FormField_typeId_fkey" FOREIGN KEY ("typeId") REFERENCES "FormFieldType" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "FormField_optionListId_fkey" FOREIGN KEY ("optionListId") REFERENCES "OptionList" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+INSERT INTO "new_FormField" ("createdAt", "id", "label", "name", "optionListId", "position", "required", "stepId", "typeId", "updatedAt") SELECT "createdAt", "id", "label", "name", "optionListId", "position", "required", "stepId", "typeId", "updatedAt" FROM "FormField";
+DROP TABLE "FormField";
+ALTER TABLE "new_FormField" RENAME TO "FormField";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
