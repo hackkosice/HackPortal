c5aa1d25a81a2e3d312c583696cdc00fc95f39d6
Matej Tarca <matej.tarca@kiwi.com>
2023-11-20 17:54:48 +0100

    fix(sentry): fix sentry env variables

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -4,9 +4,9 @@
 
 import * as Sentry from "@sentry/nextjs";
 
-if (process.env.SENTRY_ENABLED === "true") {
+if (process.env.NEXT_PUBLIC_SENTRY_ENABLED === "true") {
   Sentry.init({
-    dsn: process.env.SENTRY_DSN,
+    dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
     // Adjust this value in production, or use tracesSampler for greater control
     tracesSampleRate: 1,
