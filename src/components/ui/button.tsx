853f55721a6f00b7e542849ed0806b99fb4e0e09
Matej Tarca <matej.tarca@kiwi.com>
2023-11-22 10:36:27 +0100

    chore(app): tweak input font-size

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -29,7 +29,7 @@ const buttonVariants = cva(
         unstyled: "",
         link: "text-hkOrange underline-offset-4 hover:underline dark:text-slate-50 p-0 m-0 h-fit w-fit",
         combobox:
-          "w-full rounded-md border border-slate-200 bg-white px-3 py-2 text-sm placeholder:text-slate-500 focus:outline-none focus:ring-2 focus:ring-hkOrange disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:bg-slate-950 dark:ring-offset-slate-950 dark:placeholder:text-slate-400 dark:focus:ring-slate-300",
+          "w-full rounded-md border border-slate-200 bg-white px-3 py-2 text-base placeholder:text-slate-500 focus:outline-none focus:ring-2 focus:ring-hkOrange disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:bg-slate-950 dark:ring-offset-slate-950 dark:placeholder:text-slate-400 dark:focus:ring-slate-300",
       },
     },
     defaultVariants: {
