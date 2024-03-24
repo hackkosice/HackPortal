00c498528f2c5c44e0cfadf7ed05c4e5da1b43f7
MatejMa2ur <matejmazur@outlook.com>
2024-03-24 13:49:27 +0100

    fix: Refactor sorting method in getLastActiveHackathonId
    Updated the "getLastActiveHackathonId" function to sort hackathons by event start date in descending order directly in the database query. This update enhances performance by eliminating the need for sorting in JavaScript and removes unnecessarily imported Prisma SortOrder.

--- src/server/getters/getLastActiveHackathonId.ts
+++ src/server/getters/getLastActiveHackathonId.ts
@@ -1,12 +1,11 @@
 import { PrismaClient } from "@prisma/client";
-import { Prisma } from ".prisma/client";
 
 const getLastActiveHackathonId = async (
   prisma: PrismaClient
 ): Promise<number | null> => {
   const latestHackathon = await prisma.hackathon.findFirst({
     orderBy: {
-      eventStartDate: Prisma.SortOrder.desc,
+      eventStartDate: "desc",
     },
   });
   return latestHackathon?.id ?? null;
