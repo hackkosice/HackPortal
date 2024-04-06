fa83b73c02c2e2c5ff412d592c459be8295a12f8
Michal Masrna <mmasrna@hackkosice.com>
2024-04-07 00:01:10 +0200

    added back button to judging results

--- src/scenes/Dashboard/scenes/Judging/scenes/JudgingResults/JudgingResults.tsx
+++ src/scenes/Dashboard/scenes/Judging/scenes/JudgingResults/JudgingResults.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import Link from "next/link";
+import { Stack } from "@/components/ui/stack";
+import { ChevronLeftIcon } from "@heroicons/react/24/outline";
 import getJudgingResults from "@/server/getters/dashboard/judging/getJudgingResults";
 
 type JudgingResultsProps = {
@@ -8,6 +11,15 @@ const JudgingResults = async ({ hackathonId }: JudgingResultsProps) => {
   const results = await getJudgingResults(hackathonId);
   return (
     <div>
+      <Link
+        href={`/dashboard/${hackathonId}/judging`}
+        className="text-hkOrange"
+      >
+        <Stack direction="row" alignItems="center" spacing="small">
+          <ChevronLeftIcon className="h-5 w-5" />
+          Back to judging
+        </Stack>
+      </Link>
       Results:{" "}
       <div>
         {results.map((result, index) => {
